d236d887255c50da9c3ef647a1dd0ee32a915e7b
octavia <itsveev@gmail.com>
2019-06-13 15:03:40 +1200

    ~w~

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d6492934ebec2f220f3d7aa7d9327c7.js"
+  "/precache-manifest.b55ac95a35e8f782b05a21a5e84f341e.js"
 );
 
 self.addEventListener('message', (event) => {
