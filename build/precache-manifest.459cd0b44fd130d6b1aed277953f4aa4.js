79959ec2e373d7e08d0b98cd756ba786e904c1f8
Sean Ryan <email@syri.moe>
2020-04-08 18:12:36 +1200

    test pt2

---
+++ build/precache-manifest.459cd0b44fd130d6b1aed277953f4aa4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e847fd489a589b11600bdc967d2372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bd796f7adb0b047402",
+    "url": "/static/css/main.748910dd.chunk.css"
+  },
+  {
+    "revision": "839c7a32eb76eda56f98",
+    "url": "/static/js/2.fcd44021.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.fcd44021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bd796f7adb0b047402",
+    "url": "/static/js/main.74a78386.chunk.js"
+  },
+  {
+    "revision": "b38f88cc27c3c60427c0",
+    "url": "/static/js/runtime-main.e3f816cb.js"
+  },
+  {
+    "revision": "0247869c4fbee341c2efae7a2ac6ad17",
+    "url": "/static/media/github.0247869c.svg"
+  },
+  {
+    "revision": "a9e33e1f98214967747568bd0336428f",
+    "url": "/static/media/twitch.a9e33e1f.svg"
+  },
+  {
+    "revision": "680c9cf1f6ff44697aed8e98e746b4bc",
+    "url": "/static/media/twitter.680c9cf1.svg"
+  },
+  {
+    "revision": "1aca2eff09dc8c7b00483984472dc867",
+    "url": "/static/media/youtube.1aca2eff.svg"
+  }
+]);
